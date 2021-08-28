d5b5a1b26e902d5704da2d1a8154f8f6197e906a
Toby Ya <35889113+fdsf53451001@users.noreply.github.com>
2021-08-29 00:26:59 +0800

    style update & structure improve

---
+++ src/js/global.js
@@ -0,0 +1,18 @@
+var point = 0;
+var last_point = 0;
+
+var ban = false;
+
+var status = 0; //0=not press 1=press
+
+// var current_cat1 = 'src/CAT21.JPG';
+// var current_cat2 = 'src/CAT22.JPG';
+// var current_catERR = 'src/CAT_ERR.JPG';
+
+var styles = [
+    ['style1','asset/photo/CAT1.JPG','asset/photo/CAT2.JPG','asset/photo/CAT_ERR.JPG'],
+    ['style2','asset/photo/CAT21.JPG','asset/photo/CAT22.JPG','asset/photo/CAT_ERR.JPG'],
+    ['style3','asset/photo/CAT31.JPG','asset/photo/CAT32.JPG','asset/photo/CAT_ERR.JPG'],
+    ['style4','asset/photo/CAT41.JPG','asset/photo/CAT42.JPG','asset/photo/CAT_ERR.JPG'],
+]
+var current_style = 0;
